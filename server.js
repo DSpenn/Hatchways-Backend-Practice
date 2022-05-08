85eff3b8d4db762678f04867df79bf2441b1aa4c
DS <derekstander@gmail.com>
2022-05-08 00:41:06 -0400

    modified:   server.js cleaned up

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 const fs = require('fs');
 const express = require('express');
 
@@ -5,33 +6,30 @@ const app = express();
 
 app.use(express.json());
 app.get('/', (req, res) => {
-  res.send(
-    'Use the API endpoint at <a href="https://localhost:3001/api">localhost:3001/api</a>'
-  );
+  res.send('Use the API endpoint at <a href="https://localhost:3001/api">localhost:3001/api</a>');
 });
 app.listen(3001, () => console.log('Express Server on port 3001!'));
 
-var parsedRecipes = [];
+let parsedRecipes = [];
 
 async function dbRead() {
   try {
-    data = fs.readFileSync('./db/recipes.json');
+    const data = fs.readFileSync('./db/recipes.json');
     parsedRecipes = JSON.parse(data);
   } catch {
     console.log(e);
   }
-  return parsedRecipes
+  return parsedRecipes;
 }
 
-async function dbWrite(parsedRecipes) {
+async function dbWrite() {
   try {
     fs.writeFile(
       './db/recipes.json',
       JSON.stringify(parsedRecipes, null, 4),
-      (writeErr) =>
-      writeErr ?
-      console.error(writeErr) :
-      console.info('Successfully updated!')
+      (writeErr) => (writeErr
+        ? console.error(writeErr)
+        : console.info('Successfully updated!')),
     );
   } catch {
     console.log(e);
@@ -41,17 +39,15 @@ async function dbWrite(parsedRecipes) {
 app.get('/recipes', (req, res) => {
   dbRead();
   const recipeNames = parsedRecipes.recipes.map((recipe) => recipe.name);
-  //console.log('recipeNames', recipeNames);
+  // console.log('recipeNames', recipeNames);
   return res.json(recipeNames);
 });
 
-//http://localhost:3001/recipes/details/scrambledEggs
+// http://localhost:3001/recipes/details/scrambledEggs
 app.get('/recipes/details/:recipeName', (req, res) => {
   const requestedRecipe = req.params.recipeName.toLowerCase();
-
   if (requestedRecipe) {
     dbRead();
-
     for (let i = 0; i < parsedRecipes.recipes.length; i++) {
       if (requestedRecipe === parsedRecipes.recipes[i].name.toLowerCase()) {
         parsedRecipes.recipes[i].numSteps = parsedRecipes.recipes[i].instructions.length;
@@ -62,30 +58,28 @@ app.get('/recipes/details/:recipeName', (req, res) => {
   return res.json('No recipe found');
 });
 
-
 app.post('/recipes', (req, res) => {
   console.info(`${req.method} request received to add a recipe`);
   const {
     name,
     ingredients,
-    instructions
+    instructions,
   } = req.body;
-
-  var found = false;
+  let found = false;
+  // eslint-disable-next-line no-sequences
   if (name, ingredients, instructions) {
     const newRecipe = {
       name,
       ingredients,
-      instructions
+      instructions,
     };
     dbRead();
-
-    let requestedRecipe = name.toLowerCase();
+    const requestedRecipe = name.toLowerCase();
     if (requestedRecipe) {
       for (let i = 0; i < parsedRecipes.recipes.length; i++) {
         if (requestedRecipe === parsedRecipes.recipes[i].name.toLowerCase()) {
           found = true;
-          let response = {
+          const response = {
             status: 'error',
             body: 'Recipe already exists',
           };
@@ -96,18 +90,14 @@ app.post('/recipes', (req, res) => {
     }
     if (!found) {
       parsedRecipes.recipes.push(newRecipe);
-
       dbWrite(parsedRecipes);
-
-      let response = {
+      const response = {
         status: 'success',
         body: newRecipe,
       };
-
       console.log(response);
       res.json(response);
     }
-
   }
 });
 
@@ -116,46 +106,38 @@ app.put('/recipes', (req, res) => {
   const {
     name,
     ingredients,
-    instructions
+    instructions,
   } = req.body;
-
-  var found = false;
-
+  let found = false;
   if (name, ingredients, instructions) {
     const newRecipe = {
       name,
       ingredients,
-      instructions
+      instructions,
     };
-
     dbRead();
-
-    let requestedRecipe = name.toLowerCase();
+    const requestedRecipe = name.toLowerCase();
     if (requestedRecipe) {
       for (let i = 0; i < parsedRecipes.recipes.length; i++) {
         if (requestedRecipe === parsedRecipes.recipes[i].name.toLowerCase()) {
           found = true;
           parsedRecipes.recipes.splice(i);
           parsedRecipes.recipes.push(newRecipe);
-
           dbWrite(parsedRecipes);
-
-          let response = {
+          const response = {
             status: 'success',
             body: newRecipe,
           };
-
           console.log(response);
           res.json(response);
         }
       }
     }
     if (!found) {
-      let response = {
+      const response = {
         status: 'error',
         body: 'Recipe does not exist"',
       };
-
       console.log(response);
       res.json(response);
     }
